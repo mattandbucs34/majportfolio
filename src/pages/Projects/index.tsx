1edd5cad1d0327f88b3f3c805f47c72ed391454d
Matt Johnson <mattandbucs@gmail.com>
2024-12-18 16:41:15 -0600

    initial implementation

---
+++ src/pages/Projects/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div>
+            <h1>Projects Page</h1>
+            {/* Add your projects here */}
+        </div>
+    );
+};
+
+export default Projects;
