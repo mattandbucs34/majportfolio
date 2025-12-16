6db2bd06e4e549a8d05d61e26952576e6a8b8cc8
Matt Johnson <mattandbucs@gmail.com>
2025-12-16 17:31:34 -0600

    update react projects

--- src/pages/helpers/react_projects.ts
+++ src/pages/helpers/react_projects.ts
@@ -1,11 +1,10 @@
 import { IProjectType } from "../../interfaces/IProjects";
-import BlocJams from '../../assets/images/bloc_jams.png';
 
 export const react_projects: IProjectType[] = [
   {
-    projectName: "Bloc Jams",
-    imagePath: BlocJams,
-    alt: "Bloc Jams Thumbnail",
-    href: "https://wizardly-almeida-2b3808.netlify.app/"
+    projectName: "Matty Chat",
+    imagePath: '',
+    alt: "Matty Chat Thumbnail",
+    href: "https://mattychat.majportfolio.com/"
   }
-]
+];
