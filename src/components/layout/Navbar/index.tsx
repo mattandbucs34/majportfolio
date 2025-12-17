b087c5e41ec2b98c59cc2f149a1b0fc134155c29
Matt Johnson <mattandbucs@gmail.com>
2025-12-16 22:39:37 -0600

    refactor mobile nav to own file

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -3,12 +3,7 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
 import Link from '@mui/material/Link';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Stack from '@mui/material/Stack';
@@ -19,12 +14,8 @@ import { useTheme } from '@mui/material/styles';
 import { NavLink } from 'react-router';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MobileNav from '@/components/layout/Navbar/MobileNav';
 
-const DRAWER_WIDTH = 240;
 
 const Navbar = () => {
 	const [isProjectsMenuOpen, setIsProjectsMenuOpen] = React.useState(false);
@@ -131,126 +122,10 @@ const Navbar = () => {
 					</Stack>
 				</Toolbar>
 			</AppBar>
-			<nav>
-				<Drawer
-					anchor={'left'}
-					variant={'temporary'}
-					open={isMobileMenuOpen}
-					onClose={handleDrawerToggle}
-					ModalProps={{
-						keepMounted: true,
-					}}
-					sx={{
-						display: { xs: 'block', md: 'none' },
-						'& .MuiDrawer-paper': { boxSizing: 'border-box', width: DRAWER_WIDTH },
-					}}
-				>
-					<Stack sx={{ textAlign: 'center' }}>
-						<Typography variant={'h4'} my={2}>MAJ Portfolio</Typography>
-						<Divider sx={{ borderColor: 'white' }} />
-						<List>
-							<ListItem>
-								<Link
-									component={NavLink}
-									to={'/'}
-									underline={'none'}
-									color={theme.palette.primary.main}
-									onClick={handleDrawerToggle}
-								>
-									<Typography>Home</Typography>
-								</Link>
-							</ListItem>
-							<ListItem
-								sx={{ p: 0, '& h3': { width: '100%' } }}
-								component={Accordion}
-							>
-								{/* <ListItemButton>
-									<Typography>Skills</Typography>
-								</ListItemButton> */}
-								<Accordion square >
-									<AccordionSummary
-										expandIcon={<ExpandMoreIcon />}
-										aria-controls={'panel1a-content'}
-										id={'panel1a-header'}
-									>
-										<Typography color={theme.palette.primary.main}>Projects</Typography>
-									</AccordionSummary>
-									<AccordionDetails>
-										<List>
-											<ListItem>
-												<Link
-													component={NavLink}
-													to={'/react_projects'}
-													underline={'none'}
-													color={theme.palette.primary.main}
-													onClick={handleDrawerToggle}
-												>
-													<Typography>React</Typography>
-												</Link>
-											</ListItem>
-											<ListItem>
-												<Link
-													component={NavLink}
-													to={'/php_projects'}
-													underline={'none'}
-													color={theme.palette.primary.main}
-													onClick={handleDrawerToggle}
-												>
-													<Typography>PHP</Typography>
-												</Link>
-											</ListItem>
-											<ListItem>
-												<Link
-													component={NavLink}
-													to={'/node_express'}
-													underline={'none'}
-													color={theme.palette.primary.main}
-													onClick={handleDrawerToggle}
-												>
-													<Typography>Node.js and Express</Typography>
-												</Link>
-											</ListItem>
-											<ListItem>
-												<Link
-													component={NavLink}
-													to={'/other_projects'}
-													underline={'none'}
-													color={theme.palette.primary.main}
-													onClick={handleDrawerToggle}
-												>
-													<Typography>Other</Typography>
-												</Link>
-											</ListItem>
-										</List>
-									</AccordionDetails>
-								</Accordion>
-							</ListItem>
-							<ListItem>
-								<Link
-									component={NavLink}
-									to={'/skills'}
-									underline={'none'}
-									color={theme.palette.primary.main}
-									onClick={handleDrawerToggle}
-								>
-									<Typography>Skills</Typography>
-								</Link>
-							</ListItem>
-							<ListItem>
-								<Link
-									component={NavLink}
-									to={'/contact'}
-									underline={'none'}
-									color={theme.palette.primary.main}
-									onClick={handleDrawerToggle}
-								>
-									<Typography>Contact</Typography>
-								</Link>
-							</ListItem>
-						</List>
-					</Stack>
-				</Drawer>
-			</nav>
+			<MobileNav
+				isMobileMenuOpen={isMobileMenuOpen}
+				handleDrawerToggle={handleDrawerToggle}
+			/>
 		</>
 	);
 };
